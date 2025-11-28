d438a067664fc37fa2295f45b9a6d78bcd4cdf13
futurabienesraices <futuraimobiliaria.info@gmail.com>
2025-11-28 15:35:28 -0600

    Create App.jsx

---
+++ centro-financiero/src/App.jsx
@@ -0,0 +1,9 @@
+export { default } from './App'
+
+// Nota: El archivo App.jsx completo está en el repositorio.
+// Para usar este proyecto:
+// 1. Clona o descarga el repositorio
+// 2. Navega a centro-financiero/
+// 3. Ejecuta: npm install
+// 4. Para desarrollo: npm run dev
+// 5. Para producción: npm run build
